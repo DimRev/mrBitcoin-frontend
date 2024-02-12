acf19402484641c9ee002b961e7655e56f6f3857
DimRev <dimarev444@gmail.com>
2024-02-12 21:06:05 +0200

    Setup routes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,1 +1,22 @@
-<template><section class="app-header">Header</section></template>
+<template>
+  <header class="app-header">
+    <div class="logo">Mr.Bitcoin</div>
+    <nav>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'contact' }">Contacts</RouterLink>
+      <RouterLink :to="{ name: 'stats' }">Stats</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+.app-header {
+  display: flex;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 10px;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeViewVue from '../views/HomeView.vue'
+import ContactsViewVue from '../views/ContactsView.vue'
+import StatsViewVue from '../views/StatsView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -8,7 +10,17 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeViewVue
-    }
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactsViewVue
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: StatsViewVue
+    },
   ]
 })
 
