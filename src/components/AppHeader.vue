df70a0613d3b7b564c2fa717a729491fc28a5288
DimRev <dimarev444@gmail.com>
2024-02-12 21:48:23 +0200

    Add exchangeRate to and user import

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,8 +6,22 @@
       <RouterLink :to="{ name: 'contact' }">Contacts</RouterLink>
       <RouterLink :to="{ name: 'stats' }">Stats</RouterLink>
     </nav>
+    <div v-if="user">
+      {{ user.name }}
+      {{ exchangeRate ? exchangeRate : 'loading...' }}
+    </div>
+    <div v-else>Please login</div>
   </header>
 </template>
+
+<script setup lang="ts">
+import { User } from '../services/userService'
+
+defineProps<{
+  user: User | null
+  exchangeRate: number | null
+}>()
+</script>
 
 <style lang="scss">
 .app-header {
