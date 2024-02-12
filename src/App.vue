bfa3941ab37620d9d5c0bf1a2104ca0d3a905437
DimRev <dimarev444@gmail.com>
2024-02-12 21:48:55 +0200

    Add querying from service at app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="app">
-    <AppHeader />
+    <AppHeader :user="user" :exchange-rate="exchangeRate" />
     <main>
       <RouterView />
     </main>
@@ -11,6 +11,21 @@
 <script setup lang="ts">
 import AppFooter from './components/AppFooter.vue'
 import AppHeader from './components/AppHeader.vue'
+import { User, userService } from './services/userService.ts'
+import { bitcoinService } from './services/bitcoinService.ts'
+import { ref, onMounted } from 'vue'
+
+const user = ref<User | null>(null)
+const exchangeRate = ref<number | null>(null)
+
+onMounted(() => {
+  user.value = userService.getUser()
+  loadExchangeRate()
+})
+
+async function loadExchangeRate() {
+  exchangeRate.value = await bitcoinService.getRate()
+}
 </script>
 
 <style lang="scss">
