51ce39d85988be69074e0ea65effebca6e393110
DimRev <dimarev444@gmail.com>
2024-02-13 16:04:59 +0200

    Add contactFilter

---
+++ src/components/ContactFilter.vue
@@ -0,0 +1,17 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="text" />
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{
+  (e: 'submit-term', term: string): void
+}>()
+const text = ref<string>('')
+function onSubmit() {
+  emit('submit-term', text.value)
+}
+</script>
