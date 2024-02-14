04e0bba2aaf841fcddd92e3b2506201bb3b2c486
DimRev <dimarev444@gmail.com>
2024-02-14 16:32:56 +0200

    Style details

--- src/views/ContactDetailsView.vue
+++ src/views/ContactDetailsView.vue
@@ -1,5 +1,8 @@
 <template>
-  <section v-if="selectedContact">
+  <section v-if="selectedContact" class="contact">
+    <div class="img-wrapper">
+      <img :src="`https://robohash.org/${selectedContact.name}.png`" />
+    </div>
     <h2>{{ selectedContact.name }}</h2>
     <h3>{{ selectedContact.email }}</h3>
     <h3>{{ selectedContact.phone }}</h3>
@@ -29,3 +32,18 @@ function onBack() {
   router.push({ name: 'contact' })
 }
 </script>
+
+<style scoped lang="scss">
+.contact {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #aaa;
+  border-radius: 8px;
+  margin-block: 12px;
+  padding-inline: 12px;
+  .img-wrapper {
+    width: 50%;
+  }
+}
+</style>
