9b38ffe952292a30696f1b45dbb12d11f6f80c75
DimRev <dimarev444@gmail.com>
2024-02-14 16:33:12 +0200

    Add img and style contact preview

--- src/components/ContactPreview.vue
+++ src/components/ContactPreview.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="img-wrapper">
+    <img :src="`https://robohash.org/${contact.name}.png`" />
+  </div>
   <h3>{{ contact.name }}</h3>
   <h4>{{ contact.email }}</h4>
   <h4>{{ contact.phone }}</h4>
@@ -21,5 +24,17 @@ h4 {
   font-size: 1.125em;
   font-weight: 600;
   padding-inline-start: 1em;
+}
+.img-wrapper {
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  img {
+    width: 77%;
+    border: 1px solid #cccccc11;
+    border-radius: 8px;
+    background-color: #ccc;
+    box-shadow: inset 1px 1px 3px 2px rgba(0, 0, 0, 0.5);
+  }
 }
 </style>
