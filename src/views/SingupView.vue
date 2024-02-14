59d0ad3153ad47235cd8ff0c156d145cbbd28639
DimRev <dimarev444@gmail.com>
2024-02-14 16:33:33 +0200

    Add signupView

---
+++ src/views/SingupView.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h3>Signup</h3>
+    <button @click="onLogin">Login</button>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '../store/userStore'
+
+const userStore = useUserStore()
+
+function onLogin() {
+  userStore.loginUser()
+}
+</script>
+
+<style scoped></style>
